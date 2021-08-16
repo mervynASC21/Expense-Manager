8ee267156b2aba2e12451bb2de030836a22eff40
mervynASC21 <87748820+mervynASC21@users.noreply.github.com>
2021-08-16 09:58:37 -0400

    update script2.js

--- script2.js
+++ script2.js
@@ -59,7 +59,7 @@ const renderCalendar = () => {
     ) {
       days += `<div class="today">${i}</div>`;
     } else {
-      days += `<div>${i}</div>`;
+      days += `<div class="day">${i}</div>`;
     }
   }
 
@@ -68,6 +68,10 @@ const renderCalendar = () => {
     monthDays.innerHTML = days;
   }
 };
+
+// day = document.querySelector(".day")
+
+// calender_days.addEventListener('click', )
 
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
@@ -79,4 +83,51 @@ document.querySelector(".next").addEventListener("click", () => {
   renderCalendar();
 });
 
+// document.querySelectorAll('.days').addEventListener('click', () => {
+//   days.array.forEach(element => {
+    
+//   });
+//   let body = document.querySelector('body');
+//   body.style.backgroundColor = 'red'
+//   renderCalendar()
+// })
+
+const calender_days = document.querySelectorAll('.days');
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+calender_days.forEach(day => {
+  day.addEventListener('click', () => {
+    const modal = document.querySelector(day.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(day => {
+  day.addEventListener('click', () => {
+    const modal = day.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
 renderCalendar();
+
